b982da8dd11ab3e517bcb09234469b73f5947b17
Meeraj Kanaparthi <meeraj.kanaparthi@teradata.com>
2021-01-06 18:54:40 +0530

    initial update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {CovalentHttpModule} from "@covalent/http";
+import {CovalentMarkdownModule} from "@covalent/markdown";
+import {CovalentBaseEchartsModule} from "@covalent/echarts/base";
+import {CovalentDynamicFormsModule} from "@covalent/dynamic-forms";
+import {CovalentHighlightModule} from "@covalent/highlight";
+import {CovalentStepsModule} from "@covalent/core/steps";
+import {CovalentLayoutModule} from "@covalent/core/layout";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    CovalentLayoutModule,
+    CovalentStepsModule,
+    // (optional) Additional Covalent Modules imports
+    CovalentHttpModule.forRoot(),
+    CovalentHighlightModule,
+    CovalentMarkdownModule,
+    CovalentDynamicFormsModule,
+    CovalentBaseEchartsModule,
     BrowserModule,
     AppRoutingModule
   ],
